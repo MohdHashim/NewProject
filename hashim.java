f9a46ee4594bd61ce84b2a0283e6beb9614ab487
Mohd Hashim <mohd.hashim@IN.valtech.com>
2018-09-18 17:38:51 +0530

    Hello

--- hashim.java
+++ hashim.java
@@ -2,7 +2,7 @@
 
 	    public static void main(String[] args) {
 	    // Prints "Hello, World" to the terminal window.
-	      System.out.println("Hello, World");
+	      System.out.println("Hello,Welcome to new World");
 			                 }
 			   
 	  
